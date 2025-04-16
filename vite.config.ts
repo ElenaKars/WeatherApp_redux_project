dbcf5e4bdf6c978e29ab9fb60b1bb5b7f913e628
ElenaKars <elena_baldina@yahoo.com>
2025-04-16 12:56:30 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import path from 'path';
+import path from "path"
+import rollupNodePolyFill from "rollup-plugin-node-polyfills"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias:
-    {
-      'assets': path.resolve(__dirname, 'src/assets'),
-      'components': path.resolve(__dirname, 'src/components'),
-      'pages': path.resolve(__dirname, 'src/pages'),
-      'styles': path.resolve(__dirname, 'src/styles'),
-      'store': path.resolve(__dirname, 'src/store')
-    }
+    alias: {
+      assets: path.resolve(__dirname, "src/assets"),
+      components: path.resolve(__dirname, "src/components"),
+      pages: path.resolve(__dirname, "src/pages"),
+      styles: path.resolve(__dirname, "src/styles"),
+      store: path.resolve(__dirname, "src/store"),
+
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+    },
   },
   server: {
     open: true,
@@ -23,5 +26,13 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "src/setupTests",
     mockReset: true,
+  },
+  define: {
+    "process.env": {},
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
   },
 })
